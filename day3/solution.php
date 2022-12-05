ad74610717afd5f88e43f496520bccde7e622d6f
Maha Bouchiba <maha.bouchiba@gmail.com>
2022-12-05 10:22:18 +0100

    add day 3

--- day3/solution.php
+++ day3/solution.php
@@ -1,6 +1,6 @@
 <?php
 
-$rucksack = file_get_contents('input.txt');
+$rucksack = file_get_contents("input.txt");
 
 $lines = explode("\n", $rucksack);
 
@@ -14,26 +14,86 @@ $alphabet= [
     'U' => 47, 'V' => 48, 'W' => 49, 'X' => 50, 'Y' => 51, 'Z' => 52,
 ]; 
 
-$score = 0;
-$array = [];
-foreach($lines as $key => $line) {
-    //supprimer les ligne vide
-    if($line == "") {
-        unset($lines[$key]);
-    }else{
-        $count=strlen($line);
-        $compartiment1= str_split(substr($line, 0, $count/2));
-        $compartiment2 = str_split(substr($line, $count/2, $count));
-        $sameItems = array_unique(array_intersect($compartiment1, $compartiment2));
-        var_dump($sameItems);
-        // replace array key from sameitem by value of alphabet
-        $sameItems = array_map(function($item) use ($alphabet) {
-            return $alphabet[$item];
-        }, $sameItems);
-       foreach($sameItems as $key => $item) {
-            $array[] = $item;
-       }
-    }
+// Part 2 : 
 
+
+// get an array of 3 lines
+ function getAnArrayWithThreeLine($lines){
+    $array = [];
+    for($i = 0; $i < count($lines); $i+=3){
+        $array[] = [$lines[$i], $lines[$i+1], $lines[$i+2]];
+    }
+    return $array;
 }
-print_r(array_sum($array));
+
+$arrayOfThree = getAnArrayWithThreeLine($lines);
+
+
+function getLetter($line1, $line2, $line3) {
+    $letter = '';
+    $line1 = str_split($line1);
+    $line2 = str_split($line2);
+    $line3 = str_split($line3);
+    foreach ($line1 as $key => $value) {
+        if(in_array($value, $line2) && in_array($value, $line3)&& in_array($value, $line1)) {
+            $letter = $value;
+        }
+    }
+    return $letter;
+}
+
+function getTheLetter($array){
+    $letters = [];
+    foreach ($array as $key => $value) {
+        $letter = getLetter($value[0], $value[1], $value[2]);
+        $letters[] = $letter;
+    }
+    return $letters;
+}
+
+$letters = getTheLetter($arrayOfThree);
+
+
+// get the value of hte letter in alphabet
+function getTheValueOfTheLetter($letters, $alphabet){
+    $values = [];
+    foreach ($letters as $key => $value) {
+        $values[] = $alphabet[$value];
+    }
+    return array_sum($values);
+}
+
+$arrayOfThree = getAnArrayWithThreeLine($lines);
+$letter = getTheLetter($arrayOfThree);
+
+$value = getTheValueOfTheLetter($letters, $alphabet);
+
+print_r("The value of the letters is : " . $value);
+
+
+// Part 1
+
+// $score = 0;
+// $array = [];
+// foreach($lines as $key => $line) {
+//     //supprimer les ligne vide
+//     if($line == "") {
+//         unset($lines[$key]);
+//     }else{
+//         $count=strlen($line);
+//         $compartiment1= str_split(substr($line, 0, $count/2));
+//         $compartiment2 = str_split(substr($line, $count/2, $count));
+//         $sameItems = array_unique(array_intersect($compartiment1, $compartiment2));
+//         var_dump($sameItems);
+//         // replace array key from sameitem by value of alphabet
+//         $sameItems = array_map(function($item) use ($alphabet) {
+//             return $alphabet[$item];
+//         }, $sameItems);
+//        foreach($sameItems as $key => $item) {
+//             $array[] = $item;
+//        }
+//     }
+
+// }
+// print_r(array_sum($array));
+
